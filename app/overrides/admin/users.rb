dd3fd82d6766af389a167acff118bebf68b855fb
Marek Buszman <mbusiq@gmail.com>
2014-02-28 12:59:50 +0100

    additional links

--- app/overrides/admin/users.rb
+++ app/overrides/admin/users.rb
@@ -2,3 +2,7 @@ Deface::Override.new(virtual_path: "spree/admin/users/index",
                      replace_contents: "[id='listing_users']",
                      partial: "spree/admin/users/users_listing",
                      name: "admin_listing_users")
+Deface::Override.new(virtual_path: "spree/admin/users/show",
+                     insert_before: "p",
+                     text: '<%= link_to t(:show_orders), orders_admin_user_path(@user) %>',
+                     name: "admin_user_orders_url")
